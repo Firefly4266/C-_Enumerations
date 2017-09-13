50fdc5e4266b7d3c9d1ba2855f6f1c721d1933ee
Firefly4266 <gaines.johnp@gmail.com>
2017-09-13 14:31:15 -0700

    Enum exercise pg 85 completed.

--- Enumerations/Program.cs
+++ Enumerations/Program.cs
@@ -8,8 +8,17 @@ namespace Enumerations
 {
     class Program
     {
+        public enum MonthsOfTheYear
+        {
+            Jan = 1, Feb = 2, Mar = 3, Apr = 4, May = 5, Jun = 6, Jul = 7, Aug = 8, Sept = 9, Oct = 10, Nov = 11, Dec =12 
+        }
         static void Main(string[] args)
         {
+            Console.Write(" Greeting user, please enter a number between 1 and 12: ");
+            var userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            var num = (MonthsOfTheYear)userInput;
+            Console.WriteLine($" {num} is the month that correspondes to the number you selected.\n");
         }
     }
 }
